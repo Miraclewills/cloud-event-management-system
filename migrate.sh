827a9a99e5d276638aad8b3c5c529878e6ea3d0e
Miraclewills <miraclechinasa53@gmail.com>
2026-03-31 06:42:35 +0000

    Rewrite migrate script

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-/var/app/venv/staging-LQM1lest/bin/python /var/app/staging/manage.py migrate --run-syncdb
-/var/app/venv/staging-LQM1lest/bin/python /var/app/staging/manage.py shell -c "
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
-"
+set -e
+PYTHON=/var/app/venv/staging-LQM1lest/bin/python
+$PYTHON /var/app/staging/manage.py migrate --run-syncdb
+$PYTHON /var/app/staging/manage.py createsuperuser --noinput --username admin --email admin@example.com || true
