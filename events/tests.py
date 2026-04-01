73025a1a2cdf292d24693ec510caca9cb8166cde
Miraclewills <miraclechinasa53@gmail.com>
2026-04-01 07:36:39 +0000

    Fix test issues - replace pytz with zoneinfo and fix passwords

--- events/tests.py
+++ events/tests.py
@@ -3,20 +3,20 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Event, Attendee, Announcement
 from datetime import datetime
-import pytz
+from zoneinfo import ZoneInfo
 
 class EventViewTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(
             username='testuser',
-            password='testpass123'
+            password='T3stP@ssw0rd!'
         )
         self.event = Event.objects.create(
             title='Test Event',
             description='Test Description',
-            start_time=datetime(2026, 6, 1, 9, 0, tzinfo=pytz.UTC),
-            end_time=datetime(2026, 6, 1, 17, 0, tzinfo=pytz.UTC),
+            start_time=datetime(2026, 6, 1, 9, 0, tzinfo=ZoneInfo('UTC')),
+            end_time=datetime(2026, 6, 1, 17, 0, tzinfo=ZoneInfo('UTC')),
             location='Test Location',
             capacity=100
         )
@@ -42,7 +42,7 @@ class EventViewTests(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_register_event_logged_in(self):
-        self.client.login(username='testuser', password='testpass123')
+        self.client.login(username='testuser', password='T3stP@ssw0rd!')
         response = self.client.get(
             reverse('register_event', args=[self.event.id])
         )
